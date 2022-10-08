6c23b0524fab066bd29432ab187bfdd965952c01
Wagner Bernardes Teixeira <wagner.teixeira@bancointer.com.br>
2022-10-07 23:02:11 -0300

    add contact list

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,19 @@
+import { LightningElement, wire } from 'lwc';
+
+import FIRST_NAME from '@salesforce/schema/Contact.FirstName';
+import LAST_NAME from '@salesforce/schema/Contact.LastName';
+import EMAIL from '@salesforce/schema/Contact.Email';
+
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+const COLUMNS = [
+    { label: 'First Name', fieldName: FIRST_NAME.fieldApiName, type: 'text' },
+    { label: 'Last Name', fieldName: LAST_NAME.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: EMAIL.fieldApiName, type: 'text' }
+];
+
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+}
